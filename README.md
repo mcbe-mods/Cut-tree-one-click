e3868e5aac685993c9c18a0aa9234a2ffdfe5663
Lete114 <me@imlete.cn>
2023-06-21 21:34:55 +0800

    chore(release): 0.0.1

---
+++ README.md
@@ -0,0 +1,26 @@
+# Cut tree one click
+
+Cutting trees in minecraft is really a waste of time
+
+<p align="center">
+<img src="pack_icon.png" style="width: 80%;">
+</p>
+
+## Download
+
+> required enable Beta API
+
+To download, you need to go to the GitHub repository [releases](https://github.com/Lete114/WebStack-Screenshot/releases)
+
+## Debugger
+
+Please see the official instructions for details: [https://github.com/Mojang/minecraft-debugger](https://github.com/Mojang/minecraft-debugger)
+
+After entering the game, you need to execute this command `/script debugger connect localhost 19144` for debugging
+
+## LICENSE
+
+Please note that the open source license is GPL-2.0 and you should strictly adhere to it.
+
+- Why is it GPL-2.0 license?
+- Because I had a lot of trouble developing this mod, and I wanted to learn from it or see how others wrote it, but all I saw was ugly or obfuscated JavaScript code, and I hope that every minecraft player or minecraft mod developer will make the source code public so that more people can learn and make the mod, making the minecraft community community more active and make minecraft more playable. Of course, I also hope that when you borrow someone else's code or other content, you mark it with the author's name, **please respect every open source mod author!**
---
+++ scripts/tree.js
@@ -0,0 +1,96 @@
+import { world, Block, EntityInventoryComponent } from '@minecraft/server'
+
+world.afterEvents.blockBreak.subscribe((e) => {
+  const blockTypeId = e.brokenBlockPermutation.type.id
+
+  const currentSlot = e.player.selectedSlot
+  /** @type {EntityInventoryComponent} */
+  const inventory = e.player.getComponent('inventory')
+  const currentSlotItem = inventory.container.getItem(currentSlot)
+
+  // The player is not stalking or not holding an axe, one of the conditions is not met will end directly
+  if (!e.player.isSneaking || !currentSlotItem.typeId.endsWith('_axe')) return
+
+  /**
+   * Store all coordinates of the same wood type
+   * @type { Set<string> }
+   */
+  const set = new Set()
+
+  const stack = [...getBlockNear(e.block)]
+
+  // Iterative processing of proximity squares
+  while (stack.length > 0) {
+    // Get from the last one (will modify the original array)
+    const block = stack.pop()
+
+    if (!block) continue
+
+    // As long as the unique identifier of the block ends with "_log"
+    // and is of the same type as the block currently destroyed by the player, the condition is met
+    if (block.typeId.endsWith('_log') && blockTypeId === block.typeId) {
+      // output: [1,2,3] => "1,2,3"
+      const pos = [block.x, block.y, block.z].toString()
+
+      // If the coordinates exist, skip this iteration and proceed to the next iteration
+      if (set.has(pos)) continue
+
+      set.add(pos)
+
+      // Get the squares adjacent to the new wood to append to the iteration stack
+      const blockNear = getBlockNear(block)
+      stack.push(...blockNear)
+    }
+  }
+
+  // Iterate through all wood coordinates and execute the setblock command to destroy
+  set.forEach((pos) => {
+    const cmd = `setblock ${pos.replaceAll(',', ' ')} air destroy`
+    e.player.runCommand(cmd)
+  })
+
+  /**
+   *
+   * @param { Block } block
+   * @param { number } [radius=1]
+   * @returns
+   */
+  function getBlockNear(block, radius = 1) {
+    const centerX = block.x
+    const centerY = block.y
+    const centerZ = block.z
+
+    /*
+    Store a 3x3 list of square objects centered on the current square coordinates
+
+    Top view: 0 is the current square, get the coordinates of all 1's
+
+    First floor
+    111
+    111
+    111
+
+    Second layer
+    111
+    101
+    111
+
+    Third layer
+    111
+    111
+    111
+    */
+    const positions = []
+
+    for (let x = centerX - radius; x <= centerX + radius; x++) {
+      for (let y = centerY - radius; y <= centerY + radius; y++) {
+        for (let z = centerZ - radius; z <= centerZ + radius; z++) {
+          // Get the list of eligible cube objects
+          positions.push(e.dimension.getBlock({ x, y, z }))
+        }
+      }
+    }
+
+    return positions
+  }
+})
